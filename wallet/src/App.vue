4071bf34b6a744fbdff4e7811916a47a0bcca4ea
Paul Schaaf <paulsimonschaaf@gmail.com>
2020-10-11 23:47:02 +0200

    removed sample code, added connect button - no logic yet

--- wallet/src/App.vue
+++ wallet/src/App.vue
@@ -1,26 +1,24 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <Wallet msg="Welcome to Your Vue.js App"/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Wallet from './components/Wallet.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Wallet
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+@import url('https://fonts.googleapis.com/css2?family=Roboto&display=swap');
+
+  * {
+    padding: 0;
+    margin: 0;
+    font-family: 'Roboto', sans-serif;
+  }
 </style>
---
+++ wallet/src/components/Wallet.vue
@@ -0,0 +1,43 @@
+<template>
+<div id="wallet-container">
+  <div id="connect-button">Connect</div>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'Wallet',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<style scoped>
+  #wallet-container {
+    height: 100vh;
+    width: 100%;
+  }
+
+  #connect-button {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    height: 50px;
+    width: 125px;
+    background: lightskyblue;
+    color: white;
+    border-radius: 5px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    cursor: pointer;
+    transition: .2s;
+    box-shadow: rgb(207, 215, 226) 6px 6px 12px, rgb(242, 242, 247) -12px -12px 24px -2px;
+  }
+
+  #connect-button:hover {
+    background: rgb(81, 183, 247);
+  }
+</style>
