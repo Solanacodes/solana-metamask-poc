8c4a549f7d087dc4043cd3698a73e054000b4d2c
Paul Schaaf <paulsimonschaaf@gmail.com>
2020-10-12 19:47:05 +0200

    created basic UI to connect to wallet and send - no logic yet

--- wallet/src/components/Wallet.vue
+++ wallet/src/components/Wallet.vue
@@ -1,14 +1,68 @@
 <template>
 <div id="wallet-container">
-  <div id="connect-button">Connect</div>
+  <div id="balance">Balance: {{balance || '--'}}</div>
+  <div v-if="state === states.WAITING_TO_CONNECT" id="connect-button" @click="connect">Connect</div>
+  <div v-else id="send-container">
+    <div id="recipient-address">Recipient Address</div>
+    <div id="amount-to-send">Amount</div>
+    <input id="address-input" type="text"/>
+    <input id="amount-input" type="text"/>
+    <input id="send-button" type="submit" value="Send"/>
+  </div>
+  
+  <div @click="send">hello</div>
 </div>
 </template>
 
 <script>
+import {ref} from 'vue';
 export default {
   name: 'Wallet',
   props: {
     msg: String
+  },
+  setup() {
+    const origin = new URL('package.json', "http://localhost:8081/").toString()
+    const snapId = `wallet_plugin_${origin}`
+
+    //eslint-disable-next-line no-undef
+    const metamask = ethereum;
+
+    const states = {
+      WAITING_TO_CONNECT: "WAITING_TO_CONNECT",
+      CONNECTED: "CONNECTED"
+    };
+
+    const state = ref(states.WAITING_TO_CONNECT);
+
+    const connect = async () => {
+      /* await metamask.send({
+        method: 'wallet_requestPermissions',
+        params: [{
+          [snapId]: {}
+        }]
+      }); */
+
+      state.value = states.CONNECTED;
+    }
+
+    const send = async () => {
+      try {
+        await metamask.send({
+          method: snapId,
+          params: [{
+            method: 'hello'
+          }]
+        })
+      } catch (err) {
+        console.error(err)
+        alert('Problem happened: ' + err.message || err)
+      }
+    }
+    
+    const balance = ref(0);
+
+    return {connect, send, states, state, balance};
   }
 }
 </script>
@@ -21,12 +75,12 @@ export default {
 
   #connect-button {
     position: absolute;
-    top: 50%;
+    top: 55%;
     left: 50%;
     transform: translate(-50%, -50%);
     height: 50px;
     width: 125px;
-    background: lightskyblue;
+    background: rgb(104, 197, 255);
     color: white;
     border-radius: 5px;
     display: flex;
@@ -38,6 +92,93 @@ export default {
   }
 
   #connect-button:hover {
-    background: rgb(81, 183, 247);
+    background: rgb(26, 167, 255);
+  }
+
+  #balance {
+    position: absolute;
+    top: 30%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    font-size: 40px;
+  }
+
+  #send-container {
+    position:absolute;
+    top: 55%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    height: 50px;
+    display: flex;
+  }
+
+  #address-input {
+    height: 100%;
+    width: 300px;
+    border: none;
+    box-shadow: rgb(207, 215, 226) 6px 6px 12px, rgb(242, 242, 247) -12px -12px 24px -2px;
+    border-radius: 5px;
+    font-size: 18px;
+    padding: 0 15px 0 15px;
+    border: 1px solid rgb(248, 248, 248);
+    box-sizing: border-box;
+  }
+
+  #address-input:focus {
+    outline-color: rgb(26, 167, 255);
+  }
+
+  #amount-input {
+    height: 100%;
+    width: 120px;
+    border: none;
+    box-shadow: rgb(207, 215, 226) 6px 6px 12px, rgb(242, 242, 247) -12px -12px 24px -2px;
+    border-radius: 5px;
+    font-size: 18px;
+    padding: 0 10px 0 10px;
+    margin-left: 10px;
+    border: 1px solid rgb(248, 248, 248);
+    box-sizing: border-box;
+  }
+
+  #amount-input:focus {
+    outline-color: rgb(26, 167, 255);
+  }
+
+  #send-button {
+    width: 125px;
+    background: rgb(104, 197, 255);
+    transform: translateY(.5px);
+    font-size: 14px;
+    color: white;
+    border-radius: 5px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    cursor: pointer;
+    margin-left: 10px;
+    box-shadow: rgb(207, 215, 226) 6px 6px 12px, rgb(242, 242, 247) -12px -12px 24px -2px;
+    transition: .3s;
+    border: none;
+  }
+
+   #send-button:hover {
+    background: rgb(26, 167, 255);
+  }
+
+  #send-button:focus {
+    outline: none;
+    background: rgb(26, 167, 255);
+  }
+
+  #recipient-address {
+    position: absolute;
+    top: -50%;
+  }
+
+  #amount-to-send {
+    position: absolute;
+    top: -50%;
+    left: 55%;
   }
 </style>
